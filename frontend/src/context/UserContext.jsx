193ddfd28914b496c11cf895caeb1aa52979cac0
Mukesh9116 <146856075+Mukesh9116@users.noreply.github.com>
2026-04-13 05:15:46 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { authDataContext } from './AuthContext'
 import axios from 'axios'
-import { userDataContext } from '../context/userContext';
+import { userDataContext } from '../context/userContext.js';
 function UserContext({ children }) {
   const [userData, setUserData] = useState("")
   const { serverUrl } = useContext(authDataContext)
